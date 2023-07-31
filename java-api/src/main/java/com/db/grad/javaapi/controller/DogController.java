3f32b97f01c90343c9c428053a15edafad71787c
Benedicta <benedicta.u01@gmail.com>
2023-07-31 16:25:03 +0100

    7/31/23

--- java-api/src/main/java/com/db/grad/javaapi/controller/DogController.java
+++ java-api/src/main/java/com/db/grad/javaapi/controller/DogController.java
@@ -2,12 +2,10 @@ package com.db.grad.javaapi.controller;
 
 import com.db.grad.javaapi.model.Dog;
 import com.db.grad.javaapi.service.DogService;
+import org.aspectj.lang.annotation.DeclareError;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,17 +15,33 @@ public class DogController {
   @Autowired
   DogService dogService;
 
-  @GetMapping("/")
-  public String getWelcome() {
-    return "Dogs API is up and running!";
+//  @GetMapping("/")
+//  public String getWelcome() {
+//    return "Dogs API is up and running!";
+//  }
+
+  @GetMapping("/return number of dogs in database")
+  public long getNoOfDogs(){
+    return dogService.getNoOfDogs();
   }
 
-  @GetMapping("/dogs")
+  @GetMapping("/return all dogs")
   public List<Dog> getAllDogs(){
     return dogService.getAllDogs();
   }
 
-  @PostMapping("/dogs")
+  @GetMapping("/return dog by name")
+  public Dog getDogByName(String dogsName){
+    return dogService.getDogByName(dogsName);
+  }
+
+  @GetMapping("/return dog by id")
+  public Dog getDogById(long uniqueId){
+    return dogService.getDogById(uniqueId);
+  }
+
+
+  @PostMapping("/add a new dog")
   public HttpStatus saveNewDog(@RequestBody Dog dog) {
     Dog result = dogService.addDog(dog);
     if (result == null) {
@@ -37,5 +51,16 @@ public class DogController {
       return HttpStatus.CREATED;
     }
   }
+
+  @DeleteMapping("/remove dog")
+  public boolean removeDog(long uniqueId){
+    return dogService.removeDog(uniqueId);
+  }
+
+  @PutMapping("/modify an existing dog")
+  public Dog updateDogDetails(Dog dogToUpdate){
+    return dogService.updateDogDetails(dogToUpdate);
+  }
+
 
 }
