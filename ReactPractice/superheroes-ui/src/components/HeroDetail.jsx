0b0792159b821708857736b38bdc464fce680124
Benedicta <benedicta.u01@gmail.com>
2023-08-01 13:53:42 +0100

    8/1/23

--- ReactPractice/superheroes-ui/src/components/HeroDetail.jsx
+++ ReactPractice/superheroes-ui/src/components/HeroDetail.jsx
@@ -3,6 +3,11 @@ import images from '../index';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
+const[counter, setCounter] = useState(0);
+const incrementCounter =()=>{
+  setCounter(counter + 1);
+}
+
 const HeroDetail = (props) => {
   return (
     <Card >
@@ -12,7 +17,8 @@ const HeroDetail = (props) => {
       <Card.Body >
         <Card.Title >Alias: {props.info.alias}</Card.Title>
         <Card.Text >Name: {props.info.name}</Card.Text>
-        <Button variant="primary">Like</Button>
+        <Card.Text >Likes: {counter}</Card.Text>
+        <Button variant="primary" onClick={incrementCounter}>Like</Button>
       </Card.Body>
     </Card>
   )
